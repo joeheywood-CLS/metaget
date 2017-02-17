bc3355f96cf25f27f828f5f6da2006f2856934a8
Joe Heywood <hjodqssh@ioe.ac.uk>
2017-02-17 17:15:52 +0000

    Added some functionality to allow for loops and merging lists

--- getMetaFromFile.R
+++ getMetaFromFile.R
@@ -1,6 +1,6 @@
 library(haven)
 library(rjson)
-
+library(dplyr)
 
 getMetaDataList <- function(f) {
     fl <- read_sav(f)
@@ -60,14 +60,83 @@ getVarFreq <- function(v) {
 }
 
 
-mapToSpec <- function(flmt) {
+mapToSpec <- function() {
     ## assign list of variable names to a vector
     ## loop through modules
     ## add new node with module name, and spec data
     ## basis for shiny app?
+    mn <- fromJSON(file = "../json/main.json")
+    spc <- fromJSON(file = "../json/spec.json")
+    for(mod in names(spc$modules)) {
+        mn <- mapSpecModuleToMeta(mn, spc$modules[[mod]], mod)
+    }
+    cat(toJSON(mn), file = "../json/main_mapped.json")
+
 }
 
-mapSpecModuleToMeta <- function(mn, mod) {
-
+mapSpecModuleToMeta <- function(mn, mod, modName) {
+    modVars <- names(mod$vars)
+    inMain <- names(mn)[which(names(mn) %in% modVars)]
+    for(v in inMain) {
+        cat(paste0(v, " "))
+        mn[[v]]$spec <- mod$vars[[v]]
+        mn[[v]]$mapped <- TRUE
+        mn$module = modName
+    }
+    mn
 }
 
+getComparisonObject <- function(a) {
+    spc <- data.frame(code = as.character(a$spec$ctg$code), spcLabel = a$spec$ctg$label,
+                      stringsAsFactors=F)
+    ma <- data.frame(code = as.character(a$ctg$code), mnLabel = a$ctg$label,
+                     frq = as.numeric(unlist(a$ctg$Freq)), stringsAsFactors=F)
+    frqTab = full_join(ma, spc)
+    list(vName = a$vName, varLabel = a$varLabel, format = a$format, frqTab = frqTab)
+}
+
+
+
+## This shoule be replicated and adjusted to other clusters of variables. But
+## for now it's just loops with an ordinal number at the end.
+
+assignToLoop <- function(root, jsn) {
+    loopNode <- list(root = root)
+    indVars <- names(jsn)[grepl(paste(root, "\\d$"), names(jsn))]
+    loopNode$labels <- rep("", length(indVars))
+    names(loopNode$labels) <- indVars
+    loopNode$ctg <- data.frame(code = jsn[[indVars[1]]]$ctg$code, stringsAsFactors = FALSE)
+    for(vr in indVars) {
+        ind <- jsn[[vr]]
+        ctg <- ctgToDF(ind$ctg) %>% select(code, Freq)
+        colnames(ctg)[2] <- vr
+        loopNode$ctg <- full_join(loopNode$ctg, ctg)
+        loopNode$labels[vr] <- ind$varLabel
+    }
+    loopNode
+}
+
+ctgToDF <- function(ctg) {
+    out <- data.frame(code = ctg$code, label = ctg$label, stringsAsFactors = FALSE)
+    if("missing" %in% names(ctg)) out$missing <- ctg$missing
+    if("Freq" %in% names(ctg)) out$Freq <- ctg$Freq
+    out
+}
+
+
+## ok. next task. Get all names from a module. Include two (at the moment) functions:
+## 1. Add loop to json list
+## 2. Add regular variable
+## -> come up with function to aggregate variable names and separate out loops:
+## eg. gsub("\\d+$", "_loop", vrbs); vrbs <- unique(vrbs)
+## start thinking about shiny app
+
+
+
+### Different sub-types of formats ###
+## Categorical
+## - Yes/No
+## - Loop Yes/No
+##
+## Other Numeric
+## - Date subvars: y/m/d (Day of week?)
